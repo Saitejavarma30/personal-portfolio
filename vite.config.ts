949745854f1eb62b0f5cef4d7e9713cb6429269f
Saiteja Varma <saitejavarmaj30@gmail.com>
2025-01-25 11:02:24 +0530

    performance optimizations

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { visualizer } from "rollup-plugin-visualizer";
+import compression from "vite-plugin-compression";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({
+      filename: "./dist/stats.html", // Output file
+      open: true, // Automatically open the report
+      gzipSize: true, // Show gzip size
+      brotliSize: true, // Show brotli size
+    }),
+    compression({ algorithm: "brotliCompress" }),
+  ],
   base: "personal-portfolio",
+  build: {
+    sourcemap: true,
+  },
 });
